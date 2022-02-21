da3981ce885de88dfad00fee88d9b90d2eacce17
Mark Marlow <theMarlzy@gmail.com>
2022-02-22 06:41:23 +1100

    added black formatter, venv

--- play.py
+++ play.py
@@ -3,22 +3,26 @@ from helper import convert_name_mlb_to_fangraphs
 from wpa import get_wpa
 import statsapi
 import logging
-logger = logging.getLogger('statsapi')
+
+logger = logging.getLogger("statsapi")
 logger.setLevel(logging.DEBUG)
 rootLogger = logging.getLogger()
 rootLogger.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
-formatter = logging.Formatter("%(asctime)s - %(levelname)8s - %(name)s(%(thread)s) - %(message)s")
+formatter = logging.Formatter(
+    "%(asctime)s - %(levelname)8s - %(name)s(%(thread)s) - %(message)s"
+)
 ch.setFormatter(formatter)
 rootLogger.addHandler(ch)
 
+
 def double_header_status(game, sched):
-    games_by_team = [g for g in sched if g['home_name'] == game['home_name']]
+    games_by_team = [g for g in sched if g["home_name"] == game["home_name"]]
     if len(games_by_team) == 1:
         return 0
-    
+
     for idx, g in enumerate(games_by_team):
-        if g['game_id'] == game['game_id']:
+        if g["game_id"] == game["game_id"]:
             return idx
 
     return
@@ -26,24 +30,27 @@ def double_header_status(game, sched):
 
 def get_wpa_for_day(date):
     output = []
-    sched = statsapi.schedule(start_date=date,end_date=date)
+    sched = statsapi.schedule(start_date=date, end_date=date)
     for game in sched:
-        double_header_flag = double_header_status(game, sched) 
-        home_team = convert_name_mlb_to_fangraphs(game['home_name'])
-        wpa = get_wpa(game['game_date'], home_team, double_header_flag, 2021)
+        double_header_flag = double_header_status(game, sched)
+        home_team = convert_name_mlb_to_fangraphs(game["home_name"])
+        wpa = get_wpa(game["game_date"], home_team, double_header_flag, 2021)
         output.append({"mlb_game": game, "wpa": wpa})
     return output
+
 
 def create_df(game_data):
     output = []
     for game in game_data:
-        wpa = game['wpa']
-        match_name = f"{game['mlb_game']['away_name']} @ {game['mlb_game']['home_name']}"
-        for log in wpa['wpa']:
+        wpa = game["wpa"]
+        match_name = (
+            f"{game['mlb_game']['away_name']} @ {game['mlb_game']['home_name']}"
+        )
+        for log in wpa["wpa"]:
             output.append({**log, "match": match_name})
     return pd.DataFrame(output)
 
 
 if __name__ == "__main__":
-    data = get_wpa_for_day('10/03/2021')
+    data = get_wpa_for_day("10/03/2021")
     create_df(data).to_excel("wpa.xlsx")
